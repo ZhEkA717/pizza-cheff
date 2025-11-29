43aa2a5b66a86f11ccb5ef5f2eb988465b199f44
Zheka <grushevskiy.yevgeniy@mail.ru>
2025-11-29 17:42:10 +0300

    feat: add images and translates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
+import {provideTransloco} from '@jsverse/transloco';
+import {TranslocoHttpLoader} from '@app/transloco-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideTransloco({
+      config: {
+        availableLangs: ['ru', 'en'],
+        defaultLang: 'ru',
+        prodMode: !isDevMode()
+      },
+      loader: TranslocoHttpLoader
+    })
   ]
 };
--- src/core/modules/landing/pages/landing/landing.component.ts
+++ src/core/modules/landing/pages/landing/landing.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TranslocoDirective} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    TranslocoDirective
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
 })
 export class LandingComponent {
 
