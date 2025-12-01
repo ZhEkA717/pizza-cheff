ca3adc2c58f676703549b739fd1b22d59463c348
Zheka <grushevskiy.yevgeniy@mail.ru>
2025-12-01 15:27:35 +0300

    feat: add base href

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'landing'
-  },
-  {
-    path: 'landing',
     loadChildren: () =>
       import('@core/modules/landing/landing.routes').then(({routes}) => routes)
   }
